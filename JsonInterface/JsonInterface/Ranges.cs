6d792913b866528f1d03a8c92356620418cd3d26
Gradin <gradin_g@yahoo.com>
2019-05-28 13:50:00 +0300

    test match range and reminig text pass

---
+++ JsonInterface/JsonInterface/Ranges.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface
+{
+    public class Ranges : IPattern
+    {
+        private readonly char start;
+        private readonly char end;
+
+
+        public Ranges(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+
+        }
+
+
+        public IMatch Match(string text)
+        {
+            return string.IsNullOrEmpty(text) || (start > text[0] || text[0] > end)
+                ? (IMatch)new FailedMatch(text)
+                : new SuccessMatch(text.Substring(1));
+        }
+    }
+}
---
+++ JsonInterfaceTest/RangeTest.cs
@@ -0,0 +1,21 @@
+﻿using JsonInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonInterfaceTest
+{
+    public class RangeTest
+    {
+        [Theory]
+        [InlineData( "fgh", "gh")]
+        public void TestMatchRange(string text, string remaining)
+        {
+            var range = new Ranges('b', 'g');
+            var match = range.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(match.RemainingText(), remaining);
+        }
+    }
+}
