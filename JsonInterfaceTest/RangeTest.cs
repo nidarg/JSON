ef6e92efaaff8c99947e8ac91154747304d26d6c
Gradin <gradin_g@yahoo.com>
2019-05-28 13:52:20 +0300

    test no matching range and remaining text

--- JsonInterfaceTest/RangeTest.cs
+++ JsonInterfaceTest/RangeTest.cs
@@ -17,5 +17,15 @@ namespace JsonInterfaceTest
             Assert.True(match.Success());
             Assert.Equal(match.RemainingText(), remaining);
         }
+
+        [Theory]
+        [InlineData("fgh", "fgh")]
+        public void TestMatchRangeFail(string text, string remaining)
+        {
+            var range = new Ranges('g', 'k');
+            var match = range.Match(text);
+            Assert.False(match.Success());
+            Assert.Equal(match.RemainingText(), remaining);
+        }
     }
 }
