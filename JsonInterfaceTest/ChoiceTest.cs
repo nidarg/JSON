f5f7124f323a292f7108aee4c62b985d790f8340
Gradin <gradin_g@yahoo.com>
2019-05-28 14:32:18 +0300

    test match choice fail

--- JsonInterfaceTest/ChoiceTest.cs
+++ JsonInterfaceTest/ChoiceTest.cs
@@ -25,7 +25,23 @@ namespace JsonInterfaceTest
             Assert.Equal(remaining, match.RemainingText());
         }
 
-        
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData("N9c", "N9c")]
+        [InlineData("ga", "ga")]
+        public void TestChoiseFail(string text, string remaining)
+        {
+            var hex = new Choices(
+                 new Ranges('0', '9'),
+                new Ranges('a', 'f'),
+                new Ranges('A', 'F')
+                );
+
+            var match = hex.Match(text);
+            Assert.False(match.Success());
+            Assert.Equal(remaining, match.RemainingText());
+        }
 
 
     }
