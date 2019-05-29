498c759d2bbcd3c7fbc5dc72eaeba78c6f12196d
Gradin <gradin_g@yahoo.com>
2019-05-29 09:55:42 +0300

    Test match sequance success

---
+++ JsonInterface/JsonInterface/Sequance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface
+{
+    public class Sequance:IPattern
+    {
+        IPattern[] patterns;
+
+
+        public Sequance(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            string textCopy = text;
+            foreach (var pattern in patterns)
+            {
+               
+                if (pattern.Match(text).Success())
+                {
+
+                    text = pattern.Match(text).RemainingText();
+                }
+                else return (IMatch)new FailedMatch(textCopy);
+
+            }
+            return (IMatch)new SuccessMatch(text);
+        }  
+
+    }
+    
+}
---
+++ JsonInterfaceTest/SequanceTest.cs
@@ -0,0 +1,32 @@
+﻿using JsonInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace JsonInterfaceTest
+{
+    public class SequanceTest
+    {
+
+        [Theory]
+        [InlineData("abcd", "d")]
+        public void TestSequanceSuccess(string text,string remaining)
+        {
+            var ab = new Sequance(
+                new Charact('a'),
+                new Charact('b')
+                );
+
+            var abc = new Sequance(
+                    ab,
+                    new Charact('c')
+                    );
+
+
+            var match = abc.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(remaining, match.RemainingText());
+        }
+
+    }
+}
