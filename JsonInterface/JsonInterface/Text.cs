c58b67e06b700219185c9bb96f969e55926dd671
Gradin <gradin_g@yahoo.com>
2019-05-31 13:16:19 +0300

    Test match on Test class success

---
+++ JsonInterface/JsonInterface/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface
+{
+    public class Text
+    {
+        readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if ((prefix == "") && (text != null))
+            {
+                return new SuccessMatch(text);
+            }
+
+            if (string.IsNullOrEmpty(text) || !text.Contains(prefix))
+                return new FailedMatch(text);
+            return new SuccessMatch(text.Substring(prefix.Length));
+        }
+    }
+}
---
+++ JsonInterfaceTest/TestClassTesting.cs
@@ -0,0 +1,25 @@
+﻿using JsonInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonInterfaceTest
+{
+    public class TestClassTesting
+    {
+        [Theory]
+        [InlineData("true", "trueX", "X")]
+        [InlineData("true", "true", "")]
+        [InlineData("", "true", "true")]
+
+        public void TestClassSuccess(string prefix, string text, string remainingText)
+        {
+            var e = new Text(prefix);
+            var match = e.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(remainingText, match.RemainingText());
+
+        }
+    }
+}
