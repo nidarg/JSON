faec75d95ea0b2aee3b114179d8d02b664347abb
Gradin <gradin_g@yahoo.com>
2019-05-31 15:04:03 +0300

    test match on OneOrMore fails

---
+++ JsonInterface/JsonInterface/OneOrMore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface
+{
+    public class OneOrMore
+    {
+        readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string textCopy = text;
+            var match = pattern.Match(text);
+            while (match.Success())
+            {
+
+                match = pattern.Match(match.RemainingText());
+                if (!match.Success())
+                    return new SuccessMatch(match.RemainingText());
+
+            }
+            return new FailedMatch(textCopy);
+        }
+    }
+}
---
+++ JsonInterfaceTest/OneOrMoreClassTest.cs
@@ -0,0 +1,67 @@
+﻿using JsonInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace JsonInterfaceTest
+{
+    public class OneOrMoreClassTest
+    {
+        [Theory]
+        [InlineData("abc", "bc")]
+        [InlineData("aaaabcd", "bcd")]
+        //[InlineData("", "")]
+        //[InlineData(null, null)]
+        //[InlineData("bcdef", "bcdef")]
+
+
+        public void TestOneOrMoreCharSuccess(string text, string remainingText)
+        {
+            var oneOrMoreChar = new OneOrMore(new Charact('a'));
+
+            var matchChar = oneOrMoreChar.Match(text);
+
+            Assert.True(matchChar.Success());
+            Assert.Equal(remainingText, matchChar.RemainingText());
+
+        }
+
+        [Theory]
+        [InlineData("12345ab123", "ab123")]
+        //[InlineData("bcd", "bcd")]
+        //[InlineData("", "")]
+        //[InlineData(null, null)]
+        //[InlineData("bcdef", "bcdef")]
+
+
+        public void TestOneOrMoreRangeSuccess(string text, string remainingText)
+        {
+
+            var oneOrMoreRange = new OneOrMore(new Ranges('0', '9'));
+
+            var matchRange = oneOrMoreRange.Match(text);
+            Assert.True(matchRange.Success());
+            Assert.Equal(remainingText, matchRange.RemainingText());
+
+        }
+
+        [Theory]
+        [InlineData("bcd", "bcd")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData("bcdef", "bcdef")]
+
+
+        public void TestOneOrMoreFail(string text, string remainingText)
+        {
+
+            var oneOrMoreChar = new OneOrMore(new Charact('a'));
+
+            var matchChar = oneOrMoreChar.Match(text);
+            Assert.False(matchChar.Success());
+            Assert.Equal(remainingText, matchChar.RemainingText());
+
+        }
+
+    }
+}
