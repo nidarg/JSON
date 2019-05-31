1ec25e9e5470d03eb8bd6f0feba7ccfe8fd0a3ee
Gradin <gradin_g@yahoo.com>
2019-05-31 14:37:05 +0300

    test match on Many class with Range as IPattern success

--- JsonInterfaceTest/ManyClassTest.cs
+++ JsonInterfaceTest/ManyClassTest.cs
@@ -19,11 +19,30 @@ namespace JsonInterfaceTest
         public void TestManyCharSuccess(string text, string remainingText)
         {
             var manyChar = new Many(new Charact('a'));
-           // var manyRange = new Many(new Ranges('0', '9'));
+           
             var matchChar = manyChar.Match(text);
-            //var matchRange = manyRange.Match(text);
+           
             Assert.True(matchChar.Success());
             Assert.Equal(remainingText, matchChar.RemainingText());
+
+        }
+
+        [Theory]
+        [InlineData("12345ab123", "ab123")]
+        [InlineData("bcd", "bcd")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData("bcdef", "bcdef")]
+
+
+        public void TestManyRangeSuccess(string text, string remainingText)
+        {
+            
+             var manyRange = new Many(new Ranges('0', '9'));
+           
+            var matchRange = manyRange.Match(text);
+            Assert.True(matchRange.Success());
+            Assert.Equal(remainingText, matchRange.RemainingText());
 
         }
     }
