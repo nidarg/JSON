d79c4413cba4b0a8a77d3f524fb762f6832eff80
Gradin <gradin_g@yahoo.com>
2019-05-31 14:34:15 +0300

    test match on Many class with pattern as new Charact success

---
+++ JsonInterfaceTest/ManyClassTest.cs
@@ -0,0 +1,30 @@
+﻿using JsonInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonInterfaceTest
+{
+    public class ManyClassTest
+    {
+        [Theory]
+        [InlineData("abc", "bc")]
+        [InlineData("aaaabcd", "bcd")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData("bcdef", "bcdef")]
+
+
+        public void TestManyCharSuccess(string text, string remainingText)
+        {
+            var manyChar = new Many(new Charact('a'));
+           // var manyRange = new Many(new Ranges('0', '9'));
+            var matchChar = manyChar.Match(text);
+            //var matchRange = manyRange.Match(text);
+            Assert.True(matchChar.Success());
+            Assert.Equal(remainingText, matchChar.RemainingText());
+
+        }
+    }
+}
